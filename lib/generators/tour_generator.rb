85dd07256655d5d1daee2d9a38e10dddff5674a4
Max Woolf <max.woolf@boxuk.com>
2013-12-06 12:01:32 +0000

    fix obvious error

--- lib/generators/tour_generator.rb
+++ lib/generators/tour_generator.rb
@@ -4,6 +4,6 @@ class TourGenerator < Rails::Generators::Base
   source_root File.expand_path("../templates", __FILE__)
 
   def generate_tour
-    copy_file "template_tour.yml", "tours/#{ controller.downcase }_tour.yml"
+    copy_file "template_tour.yml", "app/tours/#{ controller.downcase }_tour.yml"
   end
 end
